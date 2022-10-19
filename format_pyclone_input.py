81833c4b9a5fefa9dd0064d614fac104e62ec038
Brandon Wilk <bwilk777@uab.edu>
2022-10-19 13:03:06 -0500

    working converter

--- format_pyclone_input.py
+++ format_pyclone_input.py
@@ -29,6 +29,44 @@ def get_AD_index(format_str):
     return None
 
 
+def read_muts_from_vcf(vcf, sample_id, normal_id):
+    mut_dict = dict()
+    with open(vcf, "rt") as snvs_fp:
+        normal_sample_col = 9
+        somatic_sample_col = 10
+        for line in snvs_fp:
+            if line.startswith("#"):
+                if not line.startswith("#CHROM"):
+                    continue
+                else:
+                    cols = line.rstrip().split("\t")
+                    normal_sample_col = cols.index(normal_id)
+                    somatic_sample_col = cols.index(sample_id)
+
+            vcfcols = line.rstrip().split("\t")
+            # skip multiallelic sites as CNVKit doesn't use them for calculation
+            if "," in vcfcols[4]:
+                continue
+
+            ad_index = get_AD_index(vcfcols[8])
+            if ad_index:
+                # check if variant info pulled from somatic variant, else pull counts from the germline variant
+                ad_info = vcfcols[somatic_sample_col].split(":")[ad_index].split(",")
+                if ad_info[1] in [".", "0"]:
+                    ad_info = vcfcols[normal_sample_col].split(":")[ad_index].split(",")
+                variant_tuple = (vcfcols[0], vcfcols[1], vcfcols[3], vcfcols[4])
+                mut_dict[variant_tuple] = {
+                    "mutation_id": "_".join(variant_tuple),
+                    "ref_counts": int(ad_info[0]),
+                    "alt_counts": int(ad_info[1]),
+                    "Chromosome": vcfcols[0],
+                    "Start": int(vcfcols[1]),
+                    "End": int(vcfcols[1]),
+                }
+
+    return mut_dict
+
+
 if __name__ == "__main__":
 
     PARSER = argparse.ArgumentParser(
@@ -55,56 +93,26 @@ if __name__ == "__main__":
     ARGS = PARSER.parse_args(sys.argv[1:])
 
     # read in sample information
-    sample_info = pd.read_csv(ARGS.input, delimiter="\t", index_col="sample_id")
+    sample_info = pd.read_csv(ARGS.input, delimiter="\t", index_col="sample_id", header=0)
 
     # convert CNV segments file as a PyRange for easy feature intersection lookup
     # convert bi-allelic SNVs info into PyRange as well for intersection with CNV segments
     print("Converting CNVs and SNVs to PyRanges...")
     sample_cnv_segments = dict()
     sample_snvs = dict()
-    for sample, row in sample_info.iterrows():
+    for sample_id, row in sample_info.iterrows():
         # process CNV segments
-        tempdf = pd.read_csv(row.cnv_segments, delimiter="\t", index_col=False)
+        tempdf = pd.read_csv(row["cnv_segments"], delimiter="\t", index_col=False, header=0)
         # rename columns to make pyranges object
         tempdf.rename(
             columns={"chromosome": "Chromosome", "start": "Start", "end": "End"},
             inplace=True,
         )
-        sample_cnv_segments[sample] = PyRanges(tempdf)
+        sample_cnv_segments[sample_id] = PyRanges(tempdf)
 
         # process SNVs from VCF file
-        sample_snvs[sample] = dict()
-        with open(row.somatic_vcf, "rt") as snvs_fp:
-            normal_sample_col = 9
-            somatic_sample_col = 10
-            for line in snvs_fp:
-                if line.startswith("#"):
-                    if not line.startswith("#CHROM"):
-                        continue
-                    else:
-                        cols = line.rstrip().split("\t")
-                        normal_sample_col = cols.index(row.normal_id)
-                        somatic_sample_col = cols.index(str(sample))
-
-                vcfcols = line.rstrip().split("\t")
-                # skip multiallelic sites as CNVKit doesn't use them for calculation
-                if "," in vcfcols[4]:
-                    continue
-
-                ad_index = get_AD_index(vcfcols[8])
-                if ad_index:
-                    # check if variant info pulled from somatic variant, else pull counts from the germline variant
-                    ad_info = vcfcols[9].split(":")[ad_index].split(",")
-                    variant_tuple = (vcfcols[0], vcfcols[1], vcfcols[3], vcfcols[4])
-                    sample_snvs[sample][variant_tuple] = {
-                        "mutation_id": "_".join(variant_tuple),
-                        "ref_counts": int(ad_info[0]),
-                        "alt_counts": int(ad_info[1]),
-                        "Chromosome": vcfcols[0],
-                        "Start": int(vcfcols[1]),
-                        "End": int(vcfcols[1]),
-                    }
-
+        sample_snvs[sample_id] = read_muts_from_vcf(row["somatic_vcf"], sample_id, row["normal_id"])
+        
     # create a list of unique variant entries to fill in SNV counts with zeros
     # i.e., PyClone-Vi requires that all samples have all variants listed even if the variant wasn't called in the
     # given sample so we need to add those variants to a sample with values of zero to prevent in from pre-filtering
@@ -114,7 +122,7 @@ if __name__ == "__main__":
         var_set.update(snvs_dict.keys())
 
     for variant_tuple in var_set:
-        for snv_dict in sample_snvs:
+        for snv_dict in sample_snvs.values():
             if variant_tuple not in snv_dict:
                 snv_dict[variant_tuple] = {
                     "mutation_id": "_".join(variant_tuple),
@@ -134,14 +142,19 @@ if __name__ == "__main__":
     for sample in sample_snvs.keys():
         # join the PyRanges, then just work with the Pandas dataframe
         joined_df = (
-            PyRanges(pd.DataFrame(sample_snvs))
+            PyRanges(pd.DataFrame.from_dict(sample_snvs[sample].values()))
             .join(sample_cnv_segments[sample], suffix="_cnv")
             .df
         )
+        # drop chrY values b/c they confound analysis when cohort has a mix of males and females
+        # Get indexes where name column has value john
+        indexNames = joined_df[(joined_df['Chromosome'] == 'chrY') | (joined_df['Chromosome'] == 'Y')].index
+        # Delete these row indexes from dataFrame
+        joined_df.drop(indexNames , inplace=True)
         # add the sample ID as a column
         joined_df["sample_id"] = sample
         # add normal copy number values, 2 if Female, 1 on sex chromosomes if Male
-        if sample_info.loc[[sample]].Sex == "F":
+        if sample_info.at[sample, "sex"] == "F":
             joined_df["normal_cn"] = 2
         else:
             joined_df["normal_cn"] = joined_df.apply(
@@ -149,8 +162,13 @@ if __name__ == "__main__":
             )
         # rename columns needed for output
         joined_df.rename(
-            columns={"cn1_cnv": "major_cn", "cn2_cnv": "minor_cn"}, inplace=True
+            columns={"cn1": "major_cn", "cn2": "minor_cn"}, inplace=True
         )
+        # only keep rows where allele specific copy number info is available
+        joined_df = joined_df[joined_df['major_cn'].notna()]
+        # ensure copy numbers are repesented in the output as ints
+        joined_df["major_cn"] = joined_df["major_cn"].astype(int)
+        joined_df["minor_cn"] = joined_df["minor_cn"].astype(int)
 
         # append or setup results
         if not mut_df:
